3a1ab6b7ac7a32095f048427e3bb7096caa30572
Rambabu <74054909+RambabuSoyam@users.noreply.github.com>
2026-03-21 14:08:28 +0530

    Modify greeting message to include user name

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/api")
 def home():
-    return jsonify({"message": "Hello from Jenkins deployed backend!"})
+    name = request.args.get("name", "Guest")
+    return jsonify({"message": f"Hello {name}, response from backend!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
