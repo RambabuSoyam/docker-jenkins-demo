290b2d5b6bde36ef8cfccc078341e0874131f010
Rambabu <74054909+RambabuSoyam@users.noreply.github.com>
2026-03-22 15:44:59 +0530

    Update greeting message in home route

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/api")
 def home():
     name = request.args.get("name", "Guest")
-    return jsonify({"message": f"Hello {name}, 5000 kottara LOVDAY!"})
+    return jsonify({"message": f"Hello {name}, Greetings from RAJAMATHA"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
